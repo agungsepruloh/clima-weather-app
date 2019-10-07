fd264bf98dd2f9e99d35209cf73db9e9e8574473
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-10-07 22:11:30 +0700

    Creating and styling a TextField widget for text entry

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,16 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  autofocus: true,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
               ),
               FlatButton(
                 onPressed: () {},
